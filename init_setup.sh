9600b90ee14cf950b7781c9bc93326cdbb566986
Simran thakur <shivangithakur7300@gmail.com>
2024-12-12 01:39:19 +0530

    initializing project

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,8 @@
 echo [$(date)]: "START"
-echo [$(date)]: "Creating env with python 3.8 version"
-conda create --prefix ./env python=3.8 -y
+echo [$(date)]: "Creating env with python 3.10 version"
+conda create --prefix ./env python=3.10 -y
 echo [$(date)]: "activating the env"
 source activate ./env
-# echo [$(date)]: "installing required packages"
-# pip install -r requirements_dev.txt
-# echo [$(date)]: "END"
+echo [$(date)]: "installing required packages"
+pip install -r requirements.txt
+echo [$(date)]: "END"
